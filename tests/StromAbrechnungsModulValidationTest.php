b10382d7453322c0bb5b0f5be2e409d1d7a72d74
Till Brede <tb@symcon.de>
2019-09-30 13:49:47 +0200

    added validation test for StromAbrechnungsModul (PHP-66)

---
+++ tests/StromAbrechnungsModulValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class StromAbrechnungsModulValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateStromAbrechnungsModul(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateStromAbrechnungsModulModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../StromAbrechnungsModul');
+    }
+}
